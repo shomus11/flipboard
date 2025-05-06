a63228dcdacc55d880a154f4d8ea4f4ff600a6f9
DESKTOP-S5U8PFI\PC-Dfactory <rahmatmpr@rocketmail.com>
2025-05-06 17:57:54 +0700

    update: animation flip randomize but still had some glitch

--- Assets/Scripts/ShowroomManager.cs
+++ Assets/Scripts/ShowroomManager.cs
@@ -8,7 +8,7 @@ public class ShowroomManager : MonoBehaviour
     [SerializeField] List<Sprite> backgroundList;
     [SerializeField] List<SpriteMask> SpriteMasks;
     [SerializeField] SpriteRenderer spriteRendererTarget;
-
+    float baseAnimationDuration = .25f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -23,20 +23,53 @@ public class ShowroomManager : MonoBehaviour
 
     void ChangeBackground()
     {
-        StartCoroutine(ChangeBackgroundCoroutine());
+        DoAnimationSequence();
+        //StartCoroutine(ChangeBackgroundCoroutine());
     }
     IEnumerator ChangeBackgroundCoroutine()
     {
-        yield return new WaitForSeconds(3f);
+        //DoAnimationSequence();
+        //yield return new WaitForSeconds(3f);
 
-        RotateSpriteMask();
-        spriteRendererTarget.DOFade(0, .5f).From(1);
-        yield return new WaitForSeconds(.5f);
+        //RotateSpriteMask();
+        
+        //yield return new WaitForSeconds(.5f);
 
-        spriteRendererTarget.sprite = backgroundList[Random.Range(0, backgroundList.Count)];
-        spriteRendererTarget.DOFade(1, .5f).From(0);
-        yield return new WaitForSeconds(.5f);
+        //spriteRendererTarget.sprite = backgroundList[Random.Range(0, backgroundList.Count)];
+        //spriteRendererTarget.DOFade(1, .5f).From(0);
+        //yield return new WaitForSeconds(.5f);
+        yield return new WaitForSeconds(4f);
     }
+    public void DoAnimationSequence()
+    {
+        Sequence sequence = DOTween.Sequence();
+        float totalAnimationDuration = 0;
+        totalAnimationDuration+= baseAnimationDuration;
+        sequence.Insert(totalAnimationDuration, spriteRendererTarget.DOFade(0, baseAnimationDuration*3f).From(1));
+        totalAnimationDuration += baseAnimationDuration * 3f;
+        totalAnimationDuration = RotateSpriteMaskDuration(totalAnimationDuration, sequence);
+        totalAnimationDuration += baseAnimationDuration * 3f;
+        sequence.InsertCallback(totalAnimationDuration, () =>
+        {
+            spriteRendererTarget.sprite = backgroundList[Random.Range(0, backgroundList.Count)];
+        });
+        sequence.Insert(totalAnimationDuration, spriteRendererTarget.DOFade(1, baseAnimationDuration * 3f).From(0));
+
+    }
+    float RotateSpriteMaskDuration(float totalAnimationDuration,Sequence sequence)
+    {
+
+        for (int i = 0; i < SpriteMasks.Count; i++)
+        {
+            float delay = Random.Range(0f, baseAnimationDuration);
+            sequence.Insert(totalAnimationDuration + delay, SpriteMasks[i].transform.DOLocalRotate(Vector3.right * 360, baseAnimationDuration*3f, RotateMode.FastBeyond360).From(Vector3.zero).SetEase(Ease.Linear));
+        }
+        totalAnimationDuration += baseAnimationDuration * 4f;
+
+
+        return totalAnimationDuration;
+    }
+
     public void RotateSpriteMask()
     {
         for (int i = 0; i < SpriteMasks.Count; i++)
@@ -44,4 +77,5 @@ public class ShowroomManager : MonoBehaviour
             SpriteMasks[i].transform.DOLocalRotate(Vector3.right * 360, 1f, RotateMode.FastBeyond360).From(Vector3.zero).SetEase(Ease.Linear);
         }
     }
+    
 }
